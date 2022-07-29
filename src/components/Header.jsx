25ff29603586c7185d94c8228df2c506c531e319
Rory James <84684031+rjames187@users.noreply.github.com>
2022-07-29 23:46:21 +0000

    Create header component w/out style

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import * as React from 'React';
+
+export default function Header () {
+  return (
+    <header>
+      Carbonara
+      <nav>
+        <a>Need</a>
+        <a>Calculate</a>
+        <a>The Crew</a>
+      </nav>
+    </header>
+  );
+}
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,15 @@
+// Check thread
+
+import * as React from 'React';
+import Header from '../components/Header';
+
+export default function homepage () {
+  return (
+    <div>
+      <Header></Header>
+      <main>
+        
+      </main>
+    </div>
+  );
+}
