1f6fe5b671186d2e3c22df819b02f538e6c986a1
Rory James <84684031+rjames187@users.noreply.github.com>
2022-07-30 17:22:37 +0000

    Set up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
 import * as React from 'react';
-import Homepage from "./pages/Homepage"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from "./pages/Homepage";
+
 
 
 function App() {
   return (
-    <Homepage />
+    // <Routes>
+    //   <Route path="/" element={<Homepage />} />
+    // </Routes>
+    <Homepage/>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
+import ReactDom from 'react-dom'
 import { createRoot } from "react-dom/client";
+import Homepage from "./pages/Homepage";
 import App from "./App";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDom.render(
+  <App />
+   ,container
 );
+
+
+
