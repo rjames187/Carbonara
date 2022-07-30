d49feb3e510efacfd92fee67f377d848c1813522
Rory James <84684031+rjames187@users.noreply.github.com>
2022-07-30 18:03:47 +0000

    re-update index.js to react v18

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
-import ReactDom from 'react-dom'
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import Homepage from "./pages/Homepage";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const container = document.getElementById("root");
-ReactDom.render(
-  <App />
-   ,container
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 
