7ddf357b9dc69fc7755bbf47c5fd2c6833d35e74
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2022-07-30 02:14:37 +0000

    Working on bottom section of homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,32 +10,43 @@ export default function Homepage() {
       <Header></Header>
       <main>
         <section class="main-page-container">
-        <section class="page-container">
-          <div class="page-text-container">
-            <h1 class="home-title">For a better world, know your carbon footprint and reduce it</h1>
-            <a class="home-button">Reduce now</a>
-          </div>
-          <div class="page-image-container">
-            <img src="https://www.calliaweb.co.uk/wp-content/uploads/2015/10/600x400.jpg"></img>
-            
-          </div>
+          <section class="page-container">
+            <div class="page-text-container">
+              <h1 class="home-title">For a better world, know your carbon footprint and reduce it</h1>
+              <a class="home-button">Reduce now</a>
+            </div>
+            <div class="page-image-container">
+              <img src="https://www.calliaweb.co.uk/wp-content/uploads/2015/10/600x400.jpg"></img>
+            </div>
+          </section>
         </section>
-       </section>
-        
+
         <section class="main-page-container2">
           <section class="page-container">
-          
-          <div></div>
+            <h1 class="home-subtitle">For a future we imagine</h1>
           </section>
-          
+          <div class="image-row" id="image-row">
+            <img src="https://img.toolstud.io/300x240/3b5998/fff&text=+carbonera+placeholder+"></img>
+            <img src="https://img.toolstud.io/300x240/3b5998/fff&text=+carbonera+placeholder+"></img>
+            <img src="https://img.toolstud.io/300x240/3b5998/fff&text=+carbonera+placeholder+"></img>
+            <img src="https://img.toolstud.io/300x240/3b5998/fff&text=+carbonera+placeholder+"></img>
+          </div>
+          <div></div>
         </section>
-        
-        <section>
-          <h1>Best in class features</h1>
-          <div></div>
-          <div></div>
-          <div></div>
+
+        <section class="main-page-container">
+          <section class="page-container">
+            <div class="page-text-container">
+              <h1 style={{ color: "black" }} class="home-subtitle">For a better world, know your carbon footprint and reduce it</h1>
+              <a class="home-button">Reduce now</a>
+            </div>
+            <div class="page-image-container">
+              <img src="https://www.calliaweb.co.uk/wp-content/uploads/2015/10/600x400.jpg"></img>
+            </div>
+          </section>
         </section>
+
+
       </main>
       <Footer></Footer>
     </div>
